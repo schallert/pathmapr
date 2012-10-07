0a05ea689bca2ca1dc0528f82028ea0e4c8e667c
Matt Schallert <mattschallert@gmail.com>
2012-10-06 23:25:10 -0400

    No calls to doItAll()

--- js/ui.js
+++ js/ui.js
@@ -4,14 +4,14 @@ $("#origin-input").focusin(function () {
 	$("#dest-group").fadeIn("slow");
 });
 
-$("#origin-input").focusout(function () {
-	var address = $(this).val();
-	var geocoder = new google.maps.Geocoder();
-	geocoder.geocode({ "address": address }, function (results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-        doItAll(results[0].geometry.location);
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-	});
-});
+// $("#origin-input").focusout(function () {
+// 	var address = $(this).val();
+// 	var geocoder = new google.maps.Geocoder();
+// 	geocoder.geocode({ "address": address }, function (results, status) {
+// 		if (status == google.maps.GeocoderStatus.OK) {
+//         doItAll(results[0].geometry.location);
+//       } else {
+//         alert("Geocode was not successful for the following reason: " + status);
+//       }
+// 	});
+// });
